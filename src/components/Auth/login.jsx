fb3e85e18f6b3bc8b13ca4a53d6c8b8992dc7461
ajay <ajaykumar290904@gmail.com>
2024-11-12 06:05:22 +0530

    Login page

--- src/components/Auth/login.jsx
+++ src/components/Auth/login.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
-    <div className='flex items-center justify-center'> 
-      
-      <div className='border-2 border-red-600'>
-
-        <form>
-            
-
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="  border-2 rounded-xl border-emerald-600 p-20">
+        <form className="flex flex-col items-center justify-center">
+          <input
+          required className="text-white outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 rounded-full text-xl placeholder:text-grey-400"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+            required className="text-white outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 rounded-full text-xl mt-5 placeholder:text-grey-400"
+            type="password"
+            placeholder="Enter password"
+          />
+          <button className="text-white outline-none border-none border-2 bg-emerald-600 py-4 px-5 rounded-full text-xl mt-5 placeholder:text-white">
+            Login
+          </button>
         </form>
-
       </div>
-
     </div>
-  )
+  );
 }
