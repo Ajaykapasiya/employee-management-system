f424ece0150103658e35de5e100c338b942b8814
ajay <ajaykumar290904@gmail.com>
2024-11-15 11:27:11 +0530

    Make some changes

--- src/other/TaskList.jsx
+++ src/other/TaskList.jsx
@@ -6,15 +6,12 @@ const TaskList = () => {
     <div className='flex  mt-10 justify-between gap-5 screen '>
       <div className='h-40 w-[45%] bg-red-400 '>
         <h2 className='text-2xl font-semibold'>0</h2>
-<<<<<<< HEAD
-        <h3 className='text-xl font-medium'>New task</h3>
+        <h3 className='text-xl font-medium'>New Task</h3>
       </div>
 
     </div>
   )
+
 }
 
-=======
-      
->>>>>>> 25f8d1c (Some changes)
 export default TaskList
