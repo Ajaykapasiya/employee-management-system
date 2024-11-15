f0b5502bb40e301df2328697e32754e654488fef
ajay <ajaykumar290904@gmail.com>
2024-11-15 11:39:16 +0530

    changes

--- src/other/TaskList.jsx
+++ src/other/TaskList.jsx
@@ -6,7 +6,7 @@ const TaskList = () => {
     <div className='flex  mt-10 justify-between gap-5 screen '>
       <div className='h-40 w-[45%] bg-red-400 '>
         <h2 className='text-2xl font-semibold'>0</h2>
-        <h3 className='text-xl font-medium'>New Task</h3>
+        <h3 className='text-xl font-medium'>Newwwww Task</h3>
       </div>
 
     </div>
