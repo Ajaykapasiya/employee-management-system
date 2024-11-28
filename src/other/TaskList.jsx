9437d4778a93a030382464644184943d7f57ebc0
ajay <ajaykumar290904@gmail.com>
2024-11-28 10:12:48 +0530

    Update the code

--- src/other/TaskList.jsx
+++ src/other/TaskList.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const TaskList = () => {
     console.log('TaskList is rendering');
   return (
-    
+    <div className='flex  mt-10 justify-between gap-5 screen '>
+      <div className='h-40 w-[45%] bg-red-400 '>
+        <h2 className='text-2xl font-semibold'>0</h2>
+        <h3 className='text-xl font-medium'>Newwwww Task</h3>
+      </div>
             
     </div>
   )
