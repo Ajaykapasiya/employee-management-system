7638865307af2634fede5df7eb42bc9eaa06fa91
ajay <ajaykumar290904@gmail.com>
2024-11-29 06:41:53 +0530

    Make employee dashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from '../../other/Header'
-import TaskList from '../../other/TaskList'
+import TaskListNumber from '../../other/TaskListNumber'
+import TaskLists from '../Tasklist/TaskLists'
+
 
 const EmployeeDashboard = () => {
+
   return (
     <div className='p-10 bg-[#1C1C1C] '>
+      
      <Header/>
-     <TaskList/>
+     <TaskListNumber/>
+     <TaskLists/>
+     
     
     </div>
   )
