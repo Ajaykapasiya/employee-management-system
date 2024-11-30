ae2f4acf48de1d16cf97ba5b93aa45493dc47380
ajay <ajaykumar290904@gmail.com>
2024-11-30 21:54:36 +0530

    Update

--- src/other/TaskListNumber.jsx
+++ src/other/TaskListNumber.jsx
@@ -29,6 +29,23 @@ const TaskListNumber = () => {
         <h3 className='text-xl font-medium'>New Task</h3>
       </div>
 
+      <div className='rounded-xl  w-[45%] py-6 px-9  bg-pink-400 '>
+        <h2 className='text-3xl font-semibold'>0</h2>
+        <h3 className='text-xl font-medium'>New Task</h3>
+      </div>
+
+
+      <div className='rounded-xl  w-[45%] py-6 px-9  bg-pink-400 '>
+        <h2 className='text-3xl font-semibold'>0</h2>
+        <h3 className='text-xl font-medium'>New Task</h3>
+      </div>
+
+
+      <div className='rounded-xl  w-[45%] py-6 px-9  bg-pink-400 '>
+        <h2 className='text-3xl font-semibold'>0</h2>
+        <h3 className='text-xl font-medium'>New Task</h3>
+      </div>
+
      
     
     </div>
