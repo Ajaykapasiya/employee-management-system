5e9d51f568d3d962fd7a928f36ca46b2fa6b7aaa
ajay <ajaykumar290904@gmail.com>
2024-12-07 17:59:56 +0530

    checked

--- src/App.jsx
+++ src/App.jsx
@@ -11,3 +11,28 @@ const App = () => {
 console.log(authData.employees)   
   
  
+
+const handleLogin = (email, password) => {
+  if (email == "admin@me.com" && password == "123") {
+    setUser('admin')
+    
+    
+  } else if (email == "user@me.com" && password == "123") {
+    setUser('employees')
+   
+f
+  } else {
+    alert("Invalid Credentails");
+  }
+};
+
+
+return (
+  <>
+    {!user ? <Login handleLogin={handleLogin} /> : ''}
+    {user == 'admin' ? <AdminDashboard/> : <EmployeeDashboard/> }
+  </>
+);
+};
+
+export default App;
