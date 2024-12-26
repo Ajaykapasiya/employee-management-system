fcfe5061a3ebe3e520e92ab1dd3e0df8b495fef2
ajay <ajaykumar290904@gmail.com>
2024-12-26 17:52:15 +0530

    Refactor Header component to dynamically display user name based on authentication state

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -4,7 +4,6 @@ import { getLocalStorage, setLocalStorage } from "../utils/localStorge";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  
   const [userData, setUserData] = useState({
     employees: [],
     admin: [], 
--- src/other/Header.jsx
+++ src/other/Header.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 
 function Header({data,handleLogout}) {
  // console.log(data);
+ const [ userName , setUserName] = useState('')
+ 
+ useEffect(() => {
+  if (!data) {
+    setUserName('Admin')
+  }
+  else {
+    setUserName(data.firstname)
+  }
+  
+}, [data]);
   
   return (
     <div className='flex items-end justify-between '>
-      <h1 className='text-2xl px-8 py-3 font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{data.firstname}</span> 👋🏻</h1>
+      <h1 className='text-2xl px-8 py-3 font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{userName}</span> 👋🏻</h1>
       <button onClick={handleLogout} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-lg'>Log out</button>
     </div>
   )
