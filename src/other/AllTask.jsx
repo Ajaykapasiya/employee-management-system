663f41def46d553bc7e5ef9b6b309dcd74ad6af8
ajay <ajaykumar290904@gmail.com>
2024-12-29 23:29:55 +0530

    Refactor AllTask component to dynamically render employee tasks using auth data

--- src/other/AllTask.jsx
+++ src/other/AllTask.jsx
@@ -1,52 +1,28 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../context/AuthProvider'
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
 
 const AllTask = () => {
- 
-    const authData = useContext(AuthContext)
-    console.log(authData); 
+  const authData = useContext(AuthContext);
 
   return (
-    <div className='bg-[#1c1c1c] p-5 rounded mt-5 h-40 overflow-auto '>
-
-        <div className='bg-red-400 py-2 px-4 mb-3 flex justify-between rounded'>
-            <h2>Ajay</h2>
-            <h3>Make a UI design </h3>
-            <h5>Status</h5>
+    <div className="bg-[#1c1c1c] p-5 rounded mt-5 h-40 overflow-auto ">
+        <div className="bg-red-400 py-2 px-4 mb-3 flex justify-between rounded">
+          <h2 className="w-1/5 bg-red-600">Employee Name </h2>
+          <h3 className="w-1/5 bg-red-600 ">New Task </h3>
+          <h5 className="w-1/5 bg-red-600 ">Active Task</h5>
+          <h5 className="w-1/5 bg-red-600 ">Completed</h5>
         </div>
-
-        <div className='bg-green-400 py-2 px-4 mb-3 flex justify-between rounded'>
-            <h2>Ajay</h2>
-            <h3>Make a UI design </h3>
-            <h5>Status</h5> 
+      {authData.employees.map(function (elem) {
+        return <div className="bg-red-400 py-2 px-4 mb-3 flex justify-between rounded">
+          <h2 className="w-1/5 bg-red-600">{elem.firstname}</h2>
+          <h3 className="w-1/5 bg-red-600 ">Make a UI design </h3>
+          <h5 className="w-1/5 bg-red-600 ">Status</h5>
+          <h5 className="w-1/5 bg-red-600 ">Status</h5>
         </div>
-
-        <div className='bg-yellow-400 py-2 px-4 mb-3 flex justify-between rounded'>
-            <h2>Ajay</h2>
-            <h3>Make a UI design </h3>
-            <h5>Status</h5>
-        </div>
-
-        <div className='bg-blue-400 py-2 px-4 mb-3 flex justify-between rounded'>
-            <h2>Ajay</h2>
-            <h3>Make a UI design </h3>
-            <h5>Status</h5>
-        </div>
-
-        <div className='bg-pink-400 py-2 px-4 mb-3 flex justify-between rounded'>
-            <h2>Ajay</h2>
-            <h3>Make a UI design </h3>
-            <h5>Status</h5>
-        </div>
-
-        <div className='bg-red-900 py-2 px-4 mb-3 flex justify-between rounded'>
-            <h2>Ajay</h2>
-            <h3>Make a UI design </h3>
-            <h5>Status</h5>
-        </div>
+      })}
       
     </div>
   )
 }
 
-export default AllTask
+export default AllTask;
