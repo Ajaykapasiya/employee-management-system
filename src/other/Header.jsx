f0a741a7af9f88d44c259ea8121672b70c70f8a5
ajay <ajaykumar290904@gmail.com>
2025-01-15 23:07:40 +0530

    Refactor Header component to improve layout and enhance user greeting display

--- src/other/Header.jsx
+++ src/other/Header.jsx
@@ -13,6 +13,13 @@ function Header({data,handleLogout}) {
   }
   
 }, [data]);
- 
+  
+  return (
+    <div className='flex items-end justify-between '>
+      <h1 className='text-2xl px-8 py-3 font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{userName}</span> 👋🏻</h1>
+      <button onClick={handleLogout} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-lg'>Log out</button>
+    </div>
+  )
+}
 
 export default Header
