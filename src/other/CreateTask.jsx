0ade5cd65133b83452b92a70b094846a1ad6ecc8
ajay <ajaykumar290904@gmail.com>
2025-03-11 15:29:59 +0530

    Add path-browserify and url-polyfill dependencies; update Header and CreateTask components for improved functionality and code clarity

--- src/other/CreateTask.jsx
+++ src/other/CreateTask.jsx
@@ -1,85 +1,91 @@
-import React, { act, useState } from "react";
+import React, { useState } from "react";
 
 const CreateTask = () => {
-
-  const [taskTittle , setTaskTittle] = useState('');
-  const [taskDescription , setTaskDescription] = useState('');
-  const [taskdate , setTaskDate] = useState('');
-  const [assignTO , setAssignTO] = useState('');
-  const [category , setCategory] = useState('');
-  
-  const [task , setTask] = useState({})
+  const [taskTittle, setTaskTittle] = useState("");
+  const [taskDescription, setTaskDescription] = useState("");
+  const [taskdate, setTaskDate] = useState("");
+  const [assignTO, setAssignTO] = useState("");
+  const [category, setCategory] = useState("");
+  const [newTask, setNewTask] = useState({});
 
   const submitHandler = (e) => {
     e.preventDefault();
     //console.log("Task created");
 
-    setTask ({ taskTittle , taskDescription , taskdate, category , active:false ,newTask:true,failed:false , completed:true})
- 
-console.log(task);
+    setNewTask({
+      taskTittle,
+      taskDescription,
+      taskdate,
+      category,
+      active: false,
+      newTask: true,
+      failed: false,
+      completed: true,
+    });
 
+    const data = JSON.parse(localStorage.getItem("employees"));
+
+    data.forEach(function (elem) {
+      if (assignTO === elem.firstname) {
+        console.log(elem);
+      }
+    });
   };
 
   return (
     <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
       <form
         onSubmit={submitHandler}
-
-        className="flex flex-wrap w-full  items-start justify-between"
+        className="flex flex-wrap w-full items-start justify-between"
       >
         <div className="w-1/2">
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5 ">Task title </h3>
-            <input 
-            value={taskTittle}
-            onChange={(e) => setTaskTittle(e.target.value)}
-              className="text-sm py-1 px-2 w-4/5 rounded outline-none  bg-transparent  border-[1px] border-gray-400 mb-4"
+            <h3 className="text-sm text-gray-300 mb-0.5">Task title</h3>
+            <input
+              value={taskTittle}
+              onChange={(e) => setTaskTittle(e.target.value)}
+              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="Make UI design"
             />
           </div>
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5 ">Date</h3>
+            <h3 className="text-sm text-gray-300 mb-0.5">Date</h3>
             <input
-             value={taskdate}
-             onChange={(e) => setTaskDate(e.target.value)}
-              className="text-sm py-1 px-2 w-4/5 rounded outline-none  bg-transparent  border-[1px] border-gray-400 mb-4"
+              value={taskdate}
+              onChange={(e) => setTaskDate(e.target.value)}
+              className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="Date"
             />
           </div>
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5 ">Assign to</h3>
+            <h3 className="text-sm text-gray-300 mb-0.5">Assign to</h3>
             <input
-             value={assignTO}
-             onChange={(e) => setAssignTO(e.target.value)}
+              value={assignTO}
+              onChange={(e) => setAssignTO(e.target.value)}
               className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
               placeholder="Employee name"
             />
           </div>
-
           <div>
-            <h3 className="text-sm text-gray-300 mb-0.5 ">Category</h3>
+            <h3 className="text-sm text-gray-300 mb-0.5">Category</h3>
             <input
-             value={category}
-             onChange={(e) => setCategory(e.target.value)}
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
               className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border-[1px] border-gray-400 mb-4"
               type="text"
-              placeholder="Design , Dev , etc"
+              placeholder="Design, Dev, etc"
             />
           </div>
         </div>
-
         <div className="w-2/5 flex flex-col items-start">
-          <h3 className="text-sm text-gray-300 mb-0.5 ">Description</h3>
+          <h3 className="text-sm text-gray-300 mb-0.5">Description</h3>
           <textarea
-          value={taskDescription}
-          onChange={(e) => setTaskDescription(e.target.value)}
-            className="w-full h-44 text-sm py-2 px-4 rounded outline-none  bg-transparent border-[1px]  border-gray-400"
-            name=""
-            id=""
+            value={taskDescription}
+            onChange={(e) => setTaskDescription(e.target.value)}
+            className="w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400"
           ></textarea>
-
           <button className="bg-emerald-500 py-3 hover:bg-emerald-600 rounded text-sm mt-4 w-full">
             Create Task
           </button>
--- src/other/Header.jsx
+++ src/other/Header.jsx
@@ -15,13 +15,15 @@ const Header = (props) => {
 //}, [data]);
   const logOutUser = () => {
     localStorage.setItem('loggedInUser' , '')
+   props.changeUser('');
+    
   }
 
 
   return (
     <div className='flex items-end justify-between '>
-      <h1 className='text-2xl px-8 py-3 font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{userName}</span> 👋🏻</h1>
-      <button onClick={handleLogout} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-lg'>Log out</button>
+      <h1 className='text-2xl px-8 py-3 font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{}</span> 👋🏻</h1>
+      <button onClick={logOutUser} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-lg'>Log out</button>
     </div>
   )
 }
