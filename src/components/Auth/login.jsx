2172ca941b120736a5ba45700c66b86c4a7c4dbe
ajay <ajaykumar290904@gmail.com>
2025-03-26 20:46:13 +0530

    Remove unused state and submit handler from Login component to simplify code

--- src/components/Auth/login.jsx
+++ src/components/Auth/login.jsx
@@ -4,14 +4,7 @@ const Login = ({handleLogin}) => {
 
  
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const submitHandler = (e) => {
-    e.preventDefault();
-    handleLogin(email , password)
-   // console.log("email is", email);
-   // console.log("password is ", password);
-  };
+ 
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="  border-2 rounded-xl border-emerald-600 p-20">
